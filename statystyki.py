9b6dc1913eaa4a490eab35c9cbf040ad95c184c5
sO0phie <147306231+sO0phie@users.noreply.github.com>
2026-03-28 18:28:06 +0100

    Create statystyki.py

---
+++ statystyki.py
@@ -0,0 +1,18 @@
+import random
+
+imie = input("")
+pieniadze = 30
+cena = random.randint(10, 15)
+atak = random.randint(5, 10)
+HP = 100
+pokonanych = 0
+klasa = input()
+nagrody = ["zepsuty miecz", "stara tarcza", "złoty zegarek", "złoty medalion", "stara książka", "but"]
+inventory = []
+at_wrog_poczatek = random.randint(2, 5)
+postac = [HP, atak]
+wrog_test = [random.randint(10, 15), at_wrog_poczatek]
+miecz = []
+ksiega_zaklec = []
+luk = []
+zaklecia = ["abcdefg","xxx","nmqiap","bywtopv","ve","awqz","xyz","co","kruam"]
